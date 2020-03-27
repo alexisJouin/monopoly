00fbde20c181b89ddcf0a39f2772f86dd5b69d05
Trouchaud <trouchaud.alexandre@outlook.fr>
2020-03-27 15:28:10 +0100

    FIX: change emplacement and model Player

--- src/app/models/Board.py
+++ src/app/models/Board.py
@@ -1,17 +1,16 @@
 import json
 
+
 class Board(object):
     def __init__(self):
-        self.cases=[]
-        
-        with open('boardData.json') as json_file:
+        self.cases = []
+
+        with open('src/ressources/boardData.json') as json_file:
             data = json.load(json_file)
             for case in data['cases']:
                 self.cases.append(case)
-                 
-        #print(self.cases)
-        
-        
 
-#b = Board()
-#print(b.cases[39])
+        # print(self.cases)
+
+# b = Board()
+# print(b.cases[39])
